8e156aad605110e7f60689e144b71f6ccbeeeb93
Redwi-v <>
2025-03-18 17:51:12 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
